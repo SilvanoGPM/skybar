69b6f7390993e3f61b989c1cc58d5baf623aef0f
SkyG0D <silvanosilvino@hotmail.com>
2022-07-20 18:02:36 -0300

    feat: add icons and update manifest.json

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ export default function Document() {
 
         <link rel="manifest" href="/manifest.json" />
 
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
         <meta
